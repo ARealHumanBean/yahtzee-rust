b6d161de13333408176ccabfcfb3ac758057bb6a
Dylan J. Burton <dylanjburton2@gmail.com>
2020-04-18 18:13:39 -0700

    Create a Dice Struct
     - Add roll dice method
     - Add new dice method

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::io;
+use std::{io, fmt};
+extern crate rand;
+use rand::distributions::{Distribution, Uniform};
 /* Yahtzee flow
 number of dice = 5
 number of rerolls = 3
@@ -12,8 +14,34 @@ struct Player {
 }
 
 impl Player {
-    fn new(name: String) -> Player{
-        Player{name: name, score: 0}
+    fn new(name: String) -> Player {
+        Player {name: name, score: 0}
+    }
+}
+
+#[derive(Debug)]
+struct Dice {
+    dice: [u8; 5]
+}
+
+impl Dice {
+    fn new(&mut self) -> Dice {
+        Dice {dice: [0;5]}
+    }
+
+    fn roll(&mut self) {
+        let mut rng = rand::thread_rng();
+        let die_range = Uniform::from(1..7);
+
+        for die in self.dice.iter_mut() {
+            *die = die_range.sample(&mut rng) as u8;
+        }
+    }
+}
+
+impl fmt::Display for Dice {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.dice[0])
     }
 }
 
@@ -49,5 +77,14 @@ fn main() {
     introduction();
     let players = get_players_count();
 
-    println!("There are {}", players);
+    if players == 1 {
+        println!("Single Player game");
+    } else {
+        println!("{} Player game", players);
+    }
+
+    let mut yahtzee_dice: Dice = Dice {dice: [0;5]};
+    println!("{:?}", yahtzee_dice);
+    yahtzee_dice.roll();
+    println!("{:?}", yahtzee_dice);
 }
